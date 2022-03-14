962cc7cee06d509aa94927da863a72d428415c98
salonyy <90759727+salonyy@users.noreply.github.com>
2022-03-14 16:22:02 +0530

    login and saved link chnages

--- yts/src/components/Login/Login.js
+++ yts/src/components/Login/Login.js
@@ -4,10 +4,14 @@ import './login.css'
 const Login=()=>{
     return (
         <div className='login'>
-           
-    
-    <h1>login</h1>
-    
+           <div id = "form_container">     
+                <br/> 
+                <input type = "text" name = "email" placeholder = "Email id" ></input>
+                <input type = "text" name = "password" placeholder = "Password" ></input>
+                <br/>
+                <button id = "Formbutton">Login</button>
+                <a href=''>Register</a>
+            </div>
         </div>
         
     )
--- yts/src/components/SavedLinks/SavedLinks.js
+++ yts/src/components/SavedLinks/SavedLinks.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './savedlinks.css'
+import {InputGroup , Text, FormControl, Button, Container, Row, Col, ListGroup, Form} from "react-bootstrap";
 
 const SavedLinks=()=>{
     return (
         <div className='savedLinks'>
-           
-    
-    <h1>SavedLinks</h1>
+            <div id = 'border'>
+                <ListGroup>
+                
+                <ListGroup.Item>Biography</ListGroup.Item>
+                <ListGroup.Item>engineer</ListGroup.Item>
+                <ListGroup.Item>company</ListGroup.Item>
+                <ListGroup.Item>writer</ListGroup.Item>
+                <ListGroup.Item>indian</ListGroup.Item>
+                </ListGroup>
+            </div>
+           <Button>Delete</Button>
+           <Button>Delete All</Button>
     
         </div>
         
